71554ad229f1c91bfbf6a495b6cb22aecf12314b
Keeyong (Max) <keeyonghan@hotmail.com>
2023-04-08 13:20:39 -0700

    Update NameGenderCSVtoRedshift.py

--- dags/NameGenderCSVtoRedshift.py
+++ dags/NameGenderCSVtoRedshift.py
@@ -56,7 +56,7 @@ dag_second_assignment = DAG(
 	dag_id = 'name_gender',
 	catchup = False,
 	start_date = datetime(2023,4,6), # 날짜가 미래인 경우 실행이 안됨
-	schedule_interval = '0 2 * * *')  # 적당히 조절
+	schedule = '0 2 * * *')  # 적당히 조절
 
 task = PythonOperator(
 	task_id = 'perform_etl',
