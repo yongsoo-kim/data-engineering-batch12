aec168763071a41ec7794dd0c97f206a7afda483
yongsoo-kim <yongsookim.com@gmail.com>
2023-04-20 19:08:45 +0900

    (feature)Change the name of DAG

--- dags/WeatherForcastInfoToRedShift.py
+++ dags/WeatherForcastInfoToRedShift.py
@@ -63,7 +63,7 @@ def load(**context):
 
 
 dag_fourth_assignment = DAG(
-    dag_id='dag_fourth_assignment',
+    dag_id='weather_forcast_info',
     start_date=datetime(2023, 4, 6),  # 날짜가 미래인 경우 실행이 안됨
     schedule='0 2 * * *',  # 적당히 조절
     max_active_runs=1,
